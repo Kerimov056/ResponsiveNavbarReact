b13536a63f55ff469e32253c8800780c5f975843
Kerimov056 <ulvisk@code.edu.az>
2023-05-10 10:56:09 -0700

    API Axios

--- src/companent/Kateqoriya/Telefonlist.jsx
+++ src/companent/Kateqoriya/Telefonlist.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import Telefon from './Telefon'
+import axios from 'axios';
 import './kataqoriya.scss'
 
 const Telefonlist = () => {
     const [telefon, setTelefon] = useState([])
-    useEffect(()=>{
-        const getTelefon = () => {
-            fetch("https://fakestoreapi.com/products").then(resopnse => resopnse.json()).then(data => setTelefon(data))
-        }
-        getTelefon()
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+            .then(response => {
+                setTelefon(response.data)
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }, []);
-    return (    
+    return (
         <>
             <div className='telefonAll'>
                 <div className='telefonAll_in'>
                     {
-                        telefon.map((item,id)=>{
+                        telefon.map((item, id) => {
                             return <Telefon key={id} imgurl={item.image} iphoneName={item.title} kapasite="128GB" iphoneColor="(Blue)" oldPrice={2499.0} newPrice={item.price} />
                         })
                     }
